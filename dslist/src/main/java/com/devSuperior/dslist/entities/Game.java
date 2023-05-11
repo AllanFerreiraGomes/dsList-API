fd86dede41181ed7b45208b1d4a8aa741b675648
AllanFerreiraGomes <allanferreiragomes1@gmail.com>
2023-05-10 22:43:51 -0300

    Insert nos games

--- dslist/src/main/java/com/devSuperior/dslist/entities/Game.java
+++ dslist/src/main/java/com/devSuperior/dslist/entities/Game.java
@@ -22,24 +22,30 @@ public class Game {
 	@Column(name = "Game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
-	private String shorDescription;
+	
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 
 	public Game() {
 
 	}
 
-	public Game(long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shorDescription, String longDescription) {
+	public Game(long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
-		this.shorDescription = shorDescription;
+		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
 
@@ -75,12 +81,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -91,12 +105,12 @@ public class Game {
 		this.imgUrl = imgUrl;
 	}
 
-	public String getShorDescription() {
-		return shorDescription;
+	public String getShortDescription() {
+		return shortDescription;
 	}
 
-	public void setShorDescription(String shorDescription) {
-		this.shorDescription = shorDescription;
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
 	}
 
 	public String getLongDescription() {
